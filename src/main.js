8ff4ecc494da04e47e31916663f4a330601029bb
lance <lance@qq.com>
2020-03-12 16:39:40 +0800

    6.1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './directives'
+import './components'
+import store from './store'
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
 
 Vue.config.productionTip = false
+
+Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    store,
+
     render: h => h(App),
     components: { App },
     template: '<App/>'
